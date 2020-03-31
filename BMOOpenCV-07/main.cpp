8b8b69184dd566d9c7d8b85f7a9431f78c410972
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-03-31 00:42:47 -0300

    Estado igual ao professor, porem com a limitação de acesso a camera:
    "This app has crashed because it attempted to access privacy-sensitive data without a usage description.  The app's Info.plist must contain an NSCameraUsageDescription key with a string value explaining to the user how the app uses this data."

--- BMOOpenCV-07/main.cpp
+++ BMOOpenCV-07/main.cpp
@@ -7,9 +7,32 @@
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
+using namespace cv;
+using namespace std;
+
+const int fps = 20;
+
+//Pode fazer captura da camera do proprio computador, ou transmissão(RTP ou RTSP)
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    Mat quadro;
+    
+    VideoCapture vid(0);
+    if (!vid.isOpened()) {
+        return -1;
+    }
+    
+    while (vid.read(quadro)) {
+        //Caso t=preferisse usar um while(true), basta descomentar a linha abaixo:
+        //vid >> quadro;
+        imshow("webcam", quadro);
+        if (waitKey(1000/fps) >= 0) { //será a pelo menos 'n' fps
+            break;
+        }
+    }
+    
+    
+    return 1;
 }
